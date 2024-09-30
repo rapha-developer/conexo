dabbc156166b63d61139472a544b9e485ad855f3
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-09-30 20:19:15 -0300

    data: export conexoData constant. Responsible for stored all four playable examples (conexo game) of the application

---
+++ src/data/mocks/list/conexoData.ts
@@ -0,0 +1,12 @@
+import { ConexoType } from "../../../types/ConexoType";
+import { conexoExample1 } from "../conexo1";
+import { conexoExample2 } from "../conexo2";
+import { conexoExample3 } from "../conexo3";
+import { conexoExample4 } from "../conexo4";
+
+export const conexoData: ConexoType[] = [
+    conexoExample1,
+    conexoExample2,
+    conexoExample3,
+    conexoExample4
+]

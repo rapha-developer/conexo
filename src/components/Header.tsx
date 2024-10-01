8a91766a4ceb4bf318227df701aeec1d1a03d710
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-10-01 10:11:49 -0300

    feat: import useLocation function. And show the link back to the homepage when you are on a different page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
-
+import { NavLink, useLocation } from "react-router-dom"
+import { ArrowLeftIcon } from "../assets/arrowLeftIcon"
 
 export const Header = ({text}: { text: string }) => {
+    const location = useLocation()
     return (
         <header className="py-8">
-            <div className="container mx-auto">
+            <div className="container mx-auto relative">
+                {location.pathname !== '/' &&
+                <NavLink to={'/'}>
+                    <div className="absolute top-0 left-0"
+                    >
+                        <p className="flex items-center gap-2 font-nunito text-xl font-semibold text-darkText-100 hover:scale-105 hover:text-white"><ArrowLeftIcon /> Voltar</p>
+                    </div>
+                </NavLink>
+                }
                 <h1 className="font-nunito text-2xl lg:text-3xl font-extrabold text-darkText-100 uppercase text-center">
                     {text}
                 </h1>
@@ -11,3 +21,4 @@ export const Header = ({text}: { text: string }) => {
         </header>
     )
 }
+

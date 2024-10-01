c651db787ae9c64cb088fe7e8f74de3e11596fe6
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-10-01 10:15:22 -0300

    refactor: add different ids for each part needed for the game to work properly

--- src/utils/generateConexoPieces.ts
+++ src/utils/generateConexoPieces.ts
@@ -5,21 +5,25 @@ export function generateConexoPieces(conexo: ConexoType) {
     
     pieces.push(
         {
+            id: 0,
             value: conexo.groupOne.wordFirst,
             group: 1,
             status: 0
         },
         {
+            id: 1,
             value: conexo.groupOne.wordSecond,
             group: 1,
             status: 0
         },
         {
+            id: 2,
             value: conexo.groupOne.wordThird,
             group: 1,
             status: 0
         },
         {
+            id: 3,
             value: conexo.groupOne.wordFourth,
             group: 1,
             status: 0
@@ -28,21 +32,25 @@ export function generateConexoPieces(conexo: ConexoType) {
 
     pieces.push(
         {
+            id: 4,
             value: conexo.groupTwo.wordFirst,
             group: 2,
             status: 0
         },
         {
+            id: 5,
             value: conexo.groupTwo.wordSecond,
             group: 2,
             status: 0
         },
         {
+            id: 6,
             value: conexo.groupTwo.wordThird,
             group: 2,
             status: 0
         },
         {
+            id: 7,
             value: conexo.groupTwo.wordFourth,
             group: 2,
             status: 0
@@ -51,21 +59,25 @@ export function generateConexoPieces(conexo: ConexoType) {
 
     pieces.push(
         {
+            id: 8,
             value: conexo.groupThree.wordFirst,
             group: 3,
             status: 0
         },
         {
+            id: 9,
             value: conexo.groupThree.wordSecond,
             group: 3,
             status: 0
         },
         {
+            id: 10,
             value: conexo.groupThree.wordThird,
             group: 3,
             status: 0
         },
         {
+            id: 11,
             value: conexo.groupThree.wordFourth,
             group: 3,
             status: 0
@@ -74,21 +86,25 @@ export function generateConexoPieces(conexo: ConexoType) {
 
     pieces.push(
         {
+            id: 12,
             value: conexo.groupFour.wordFirst,
             group: 4,
             status: 0
         },
         {
+            id: 13,
             value: conexo.groupFour.wordSecond,
             group: 4,
             status: 0
         },
         {
+            id: 14,
             value: conexo.groupFour.wordThird,
             group: 4,
             status: 0
         },
         {
+            id: 15,
             value: conexo.groupFour.wordFourth,
             group: 4,
             status: 0
